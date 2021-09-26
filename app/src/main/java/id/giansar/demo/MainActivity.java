aa4baa2efc2be2bb378372908bfa8e31dc1fc489
Giansar Pratiknya <gpratiknya@gmail.com>
2021-09-26 17:15:34 +0700

    first commit

---
+++ app/src/main/java/id/giansar/demo/MainActivity.java
@@ -0,0 +1,19 @@
+package id.giansar.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setBackground(null);
+    }
+}
